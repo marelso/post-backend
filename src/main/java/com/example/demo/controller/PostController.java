4797787a42b3d6c5a5ada3c9ca1c5a5e24da5d85
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 11:09:13 -0300

    Including post controller with get method

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.PostService;
+import com.example.demo.dto.PostDto;
+import com.example.demo.dto.factory.PostFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService service;
+    private final PostFactory factory;
+
+    @GetMapping
+    private Page<PostDto> get(@ApiIgnore Pageable pageable) {
+        return service.list(pageable).map(factory::from);
+    }
+}
