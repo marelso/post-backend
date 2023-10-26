99046902f09f3d723b2af38d2fd098e9a54520c7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 11:38:01 -0300

    Including column import

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
 import java.util.Date;
 
 @Getter
