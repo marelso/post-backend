c74bf50883c71a7d5242c95c198236de0c62690e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 10:08:40 -0300

    Updating Swagger config

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -7,26 +7,39 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public Docket offersApi() {
+    public Docket postApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("")
+                .groupName("V1")
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(info());
     }
 
-    private ApiInfo apiInfo() {
+    private ApiInfo info() {
         return new ApiInfoBuilder()
-                .title("Aqui funciona")
+                .title("Post API Documentation")
+                .description("API documentation for the Post application.")
+                .version("1.0.0")
+                .termsOfServiceUrl("")
+                .contact(getContactInfo())
                 .build();
+    }
+
+    private Contact getContactInfo() {
+        return new Contact(
+                "Marelso",
+                "https://www.linkedin.com/in/marelso/",
+                "klein.marcelojr@gmail.com"
+        );
     }
 
 }
