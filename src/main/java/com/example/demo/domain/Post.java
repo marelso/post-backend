1b3e913afd825b6c3ee4d7fedda46d91660bcd52
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 10:25:46 -0300

    Including new entity

---
+++ src/main/java/com/example/demo/domain/Post.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String headline;
+    private String description;
+    private String content;
+    private String previewImage;
+    private String bannerImage;
+    private Boolean status;
+    @CreatedDate
+    private Date createdAt;
+    @LastModifiedDate
+    private Date lastModifiedDate;
+}
